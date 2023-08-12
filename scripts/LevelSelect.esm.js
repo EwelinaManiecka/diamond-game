af5c8621566e9cae0d1cc45caad095fea3c9332e
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-08-12 12:51:13 +0200

    add buttons wint level game, other vision - with levels

--- scripts/LevelSelect.esm.js
+++ scripts/LevelSelect.esm.js
@@ -1,5 +1,41 @@
-import { Common } from "./Common.esm.js";
+import { Common, HIDDEN_CLASS, HIDDEN_SCREEN } from "./Common.esm.js";
 
-class LevelSelect extends Common {}
+const LEVEL_SELECT_ID = "js-level-select-screen";
+const LEVEL_SELECT_BUTTON_ID = "level-select__button";
+
+const gameLevels = [
+  {
+    level: 1,
+  },
+  {
+    level: 2,
+  },
+  {
+    level: 3,
+  },
+];
+
+class LevelSelect extends Common {
+  constructor() {
+    super(LEVEL_SELECT_ID);
+    gameLevels.forEach((gameLevel) => this.createButton(gameLevel.level));
+  }
+
+  createButton(value) {
+    const button = document.createElement("button");
+
+    button.type = "button";
+    button.classList.add(LEVEL_SELECT_BUTTON_ID);
+    button.textContent = value;
+    button.addEventListener("click", (event) =>
+      this.buttonOnClickHandler(event)
+    );
+    this.element.appendChild(button);
+  }
+
+  buttonOnClickHandler(event) {
+    this.changeVisibilityScreen(this.element, HIDDEN_SCREEN);
+  }
+}
 
 export const levelSelect = new LevelSelect();
