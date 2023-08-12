304f874f20b05830639a288a669a63a7e50f339b
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-08-12 11:50:11 +0200

    add file LevelSelect and add scale game

---
+++ scripts/LevelSelect.esm.js
@@ -0,0 +1,5 @@
+import { Common } from "./Common.esm.js";
+
+class LevelSelect extends Common {}
+
+export const levelSelect = new LevelSelect();
--- scripts/MainMenu.esm.js
+++ scripts/MainMenu.esm.js
@@ -3,11 +3,14 @@ import { Common, HIDDEN_SCREEN, VISIBLE_SCREEN } from "./Common.esm.js";
 const START_SCREEN_ID = "js-start-screen";
 const START_SCREEN_GAME_BUTTON_ID = "js-start-game";
 const START_SCREEN_SETTINGS_BUTTON_ID = "js-settings-button";
+const SCALE_PROPERTY = "--scale-value";
 
 class MainMenu extends Common {
   constructor() {
     super(START_SCREEN_ID);
     this.bindToGameElements();
+    this.resizeGameWindow();
+    window.addEventListener("resize", this.resizeGameWindow);
   }
 
   bindToGameElements() {
@@ -25,6 +28,14 @@ class MainMenu extends Common {
   }
   showSettingsScreen() {
     console.log("Ustawienia gry");
+  }
+
+  resizeGameWindow() {
+    const { innerWidth: width, innerHeight: height } = window;
+
+    const scale = Math.min(width / 640, height / 480);
+
+    document.documentElement.style.setProperty(SCALE_PROPERTY, scale);
   }
 }
 
